ddc2ac08904432bd1a2fb0550126118f3678a452
Giovanni Gulino <giovanni.gulino@certilogo.com>
2021-01-04 17:43:12 +0100

    Demo project

---
+++ src/test/java/com/thagul/testcbr4j/Testcbr4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thagul.testcbr4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testcbr4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
