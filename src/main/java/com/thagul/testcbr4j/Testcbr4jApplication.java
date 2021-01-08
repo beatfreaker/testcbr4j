2a7b4154add10675b8589fbebda48207a6f61ac7
Chintan Radia <radiachints1512@gmail.com>
2021-01-09 00:58:03 +0530

    Create circuitBreakerFeignBuilder bean

--- src/main/java/com/thagul/testcbr4j/Testcbr4jApplication.java
+++ src/main/java/com/thagul/testcbr4j/Testcbr4jApplication.java
@@ -1,8 +1,11 @@
 package com.thagul.testcbr4j;
 
+import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class Testcbr4jApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Testcbr4jApplication.class, args);
+    }
+    
+    @Bean
+    public Feign.Builder circuitBreakerFeignBuilder() {
+        return FeignCircuitBreaker.builder();
     }
 
 }
